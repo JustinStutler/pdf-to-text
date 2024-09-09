7bd7e3032b47f77da8c38f352c00c90d7ad3c962
Wavay <stutlerjustin@gmail.com>
2024-09-09 14:18:28 -0400

    abstracted file name and added helpful comments
    -comments will guide the user on how to use the code

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ from pdf2image import convert_from_path
 from PIL import Image
 import pytesseract
 
+### HOW TO USE
+# Copy and Paste the name of the file in between the '' below
+# EXAMPLE
+# name = 'fileName'
+# DO NOT inculde the file tag such as .pdf
+
+name = ''
+
 # images [] stores images from pdf
-images = convert_from_path('text.pdf')
+images = convert_from_path(name + '.pdf')
 
 # create list [] to store text
 textList = []
 
 for image in images:
-    # grab text from images using OCR
-    # Use Tesseract to do OCR on the image
+    # grab text from images using Tesseract's OCR
     text = pytesseract.image_to_string(image)
 
     # add text to list []
@@ -21,6 +28,6 @@ for image in images:
 
 
 # Create and save text to a text file
-with open('extracted_text.txt', 'w', encoding='utf-8') as file:
+with open(name + '.txt', 'w', encoding='utf-8') as file:
     for text in textList:
         file.write(text + '\n\n')  # Add extra newline for separation between pages
